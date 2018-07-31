d76360017c69174dc84d2e41fe552d63bfad391f
Ramin <RRakhshani95@gmail.com>
2018-07-31 12:09:26 +0430

    Customer & Operator models

---
+++ Wallet.Model/User/Customer.cs
@@ -0,0 +1,16 @@
+﻿using Bit.Model.Contracts;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Wallet.Model.User
+{
+    public class Customer : IEntity
+    {
+        [Range(0, double.MaxValue)]
+        public virtual int MaxTransactionsPerDay { get; set; }
+
+        public virtual Guid UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Wallet.Model/User/Operator.cs
@@ -0,0 +1,22 @@
+﻿using Bit.Model.Contracts;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Wallet.Model.User
+{
+    public class Operator : IEntity
+    {
+        // Unique
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public virtual string UserName { get; set; }
+
+        [Required]
+        [StringLength(250, MinimumLength = 3)]
+        public virtual string Password { get; set; }
+
+        public virtual Guid UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
